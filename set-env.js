61803317b507be95b13b50b1691749f949b2e005
Gustavo Santamaria <gustavo.santamaria@rappi.com>
2020-06-22 10:53:08 -0500

    env workinprogress

---
+++ set-env.js
@@ -0,0 +1,31 @@
+
+const fs = require('file-system'); // Configure Angular `environment.ts` file path
+// require('dotenv').load(); // `environment.ts` file structure
+
+const targetPath = './src/environments/environment.ts'; // Load node modules
+const isProduction = false;
+
+const envConfigFile =
+`export const environment = {
+  production: ${isProduction},
+  firebaseConfig: {
+    apiKey: '${process.env.FIREBASE_API_KEY}',
+    authDomain: '${process.env.FIREBASE_AUTH_DOMAIN}',
+    databaseURL: '${process.env.FIREBASE_DATA_BASE_URL}',
+    projectId: '${process.env.FIREBASE_PROJECT_ID}',
+    storageBucket: '${process.env.FIREBASE_STORAGE_BUCKET}',
+    messagingSenderId: '${process.env.FIREBASE_MESSAGING_SENDER_ID}',
+    appId: '${process.env.FIREBASE_APP_ID}'
+  }
+};`;
+
+console.log('The file `environment.ts` will be written with the following content: \n');
+console.log(envConfigFile);
+
+fs.writeFile(targetPath, envConfigFile, function(err) {
+  if (err) {
+    throw console.error(err);
+  } else {
+    console.log(`Angular environment.ts file generated correctly at ${targetPath} \n`);
+  }
+});
