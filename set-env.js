80779cb4a8c475517c78bc13564d92aa47903ad7
Gustavo Santamaria <gustavo.santamaria@rappi.com>
2020-06-22 10:59:31 -0500

    env dev config

--- set-env.js
+++ set-env.js
@@ -9,15 +9,16 @@ const envConfigFile =
 `export const environment = {
   production: ${isProduction},
   firebaseConfig: {
-    apiKey: '${process.env.FIREBASE_API_KEY}',
-    authDomain: '${process.env.FIREBASE_AUTH_DOMAIN}',
-    databaseURL: '${process.env.FIREBASE_DATA_BASE_URL}',
-    projectId: '${process.env.FIREBASE_PROJECT_ID}',
-    storageBucket: '${process.env.FIREBASE_STORAGE_BUCKET}',
-    messagingSenderId: '${process.env.FIREBASE_MESSAGING_SENDER_ID}',
-    appId: '${process.env.FIREBASE_APP_ID}'
+    apiKey: ${process.env.FIREBASE_API_KEY}
+    authDomain: ${process.env.FIREBASE_AUTH_DOMAIN}
+    databaseURL: ${process.env.FIREBASE_DATA_BASE_URL}
+    projectId: ${process.env.FIREBASE_PROJECT_ID}
+    storageBucket: ${process.env.FIREBASE_STORAGE_BUCKET}
+    messagingSenderId: ${process.env.FIREBASE_MESSAGING_SENDER_ID}
+    appId: ${process.env.FIREBASE_APP_ID}
   }
-};`;
+};
+`;
 
 console.log('The file `environment.ts` will be written with the following content: \n');
 console.log(envConfigFile);
